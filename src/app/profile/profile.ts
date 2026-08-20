d57f20546f202f0218fe02cc119ab5b99c4367cf
Riddhi <22bt04084@gsfc.univercity.ac.in>
2026-08-20 10:11:08 +0530

    Day 01 Angular profile component

---
+++ src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-profile',
+  styleUrl: './profile.css',
+  templateUrl: './profile.html',
+})
+export class Profile {
+  title: string = "My Profile";
+}
