a26100fefec804b256d67307fbfdc98cfba14e09
Riddhi <22bt04084@gsfc.univercity.ac.in>
2026-08-22 12:54:02 +0530

    Add Angular Material UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink,RouterLinkActive, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
