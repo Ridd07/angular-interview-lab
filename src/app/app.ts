fe5069af9ce3000dfc8fc2ed7edaa17228a4b1dc
Riddhi <22bt04084@gsfc.univercity.ac.in>
2026-09-09 11:00:25 +0530

    task-07: Initial clean setup for Task 7

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,12 @@
 import { Component } from '@angular/core';
-import { PriorityDirective } from './priority';
-import { DurationPipe } from './duration-pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [PriorityDirective, DurationPipe],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
 
-   tasks = [
-    {
-      title: 'Angular Basics',
-      priority: 'high',
-      duration: 90
-    },
-    {
-      title: 'TypeScript',
-      priority: 'medium',
-      duration: 45
-    },
-    {
-      title: 'HTML',
-      priority: 'low',
-      duration: 30
-    }
-  ];
 }
+
--- src/app/priority.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Priority } from './priority';
-
-describe('Priority', () => {
-  it('should create an instance', () => {
-    const directive = new Priority();
-    expect(directive).toBeTruthy();
-  });
-});
